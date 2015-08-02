cb65144875d58ecac0805c6c89f82927d16e5b7f
eparkhurst <eli.parkhurst@gmail.com>
2015-08-02 17:03:53 -0600

    added prompt to change number of pixels

--- js/etch.js
+++ js/etch.js
@@ -1,21 +1,38 @@
 
+var num=16;
+var boxes = Math.pow(num,2);
 
 $(document).ready(function(){
-	$("span").hide().show("slow");
-	// var $div = $("<div class='pix'></div>");
-	for(var i=0;i<256; i++){
-		$(".container").append("<div class='pix'></div>");
-	} 
+	setup(num);
 	$(".pix").mouseenter(function(){
-		$(this).css("background-color","blue");
+		$(this).css("background-color","black");
 	});
-
+	
 	$("button").on("click", clear);
 
 });
 
+var setup = function(num){
+
+	for(var i=0;i<num; i++){
+		for(var j=0;j<num;j++){	
+			$(".container").append("<div class='pix'></div>");
+		}
+	} 
+	console.log(boxes);
+	$(".pix").css("height", 480/num);
+	$(".pix").css("width", 480/num);
+
+}
 
 
 var clear = function(){
-	$(".container div").css("background-color","red");
+	$(".container").empty();
+	var num = prompt("how many boxes across would you like");
+	
+	setup(num);
+
+	$(".pix").mouseenter(function(){
+		$(this).css("background-color","black");
+	});
 }
