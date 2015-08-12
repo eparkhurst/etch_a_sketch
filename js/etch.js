415d066f5ba41cee7a68864a18bb1356548b0695
eparkhurst <eli.parkhurst@gmail.com>
2015-08-12 10:27:04 -0600

    This is the etch a sketch prject. The initial functionality exists but it needs some work

--- js/etch.js
+++ js/etch.js
@@ -4,15 +4,21 @@ var boxes = Math.pow(num,2);
 
 $(document).ready(function(){
 	setup(num);
-	$(".pix").mouseenter(function(){
-		$(this).css("background-color","black");
-	});
+
+	// $(".pix").mouseenter(function(){
+	// 	$(this).css("background-color","black");
+	// });
+
+	$("#clear").click(clear);
 	
-	$("button").on("click", clear);
+	$(".middle #sixteen").click(setup(16));
 
 });
 
+
+
 var setup = function(num){
+	$(".container").empty();
 
 	for(var i=0;i<num; i++){
 		for(var j=0;j<num;j++){	
@@ -23,16 +29,19 @@ var setup = function(num){
 	$(".pix").css("height", 480/num);
 	$(".pix").css("width", 480/num);
 
+	$(".pix").mouseenter(function(){
+		$(this).css("background-color","black");
+	});
 }
 
 
 var clear = function(){
-	$(".container").empty();
+	
 	var num = prompt("how many boxes across would you like");
 	
 	setup(num);
 
-	$(".pix").mouseenter(function(){
-		$(this).css("background-color","black");
-	});
+	// $(".pix").mouseenter(function(){
+	// 	$(this).css("background-color","black");
+	// });
 }
